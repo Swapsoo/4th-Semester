a412bb58cebe4ca8bd66d68323a42148d24db86a
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2024-02-21 20:32:56 +0545

    Update Best_Fit.cpp

--- OS/Best_Fit.cpp
+++ OS/Best_Fit.cpp
@@ -1,68 +1,35 @@
-#include<iostream> 
-using namespace std; 
+#include<iostream>
+using namespace std;
 
-// Method to allocate memory to blocks as per Best fit algorithm 
-void bestFit(int blockSize[], int m, int processSize[], int n) 
-{ 
-	// Stores block id of the block allocated to a process 
-	int allocation[n]; 
-
-	// Initially no block is assigned to any process 
-	for (int i = 0; i < n; i++) 
-		allocation[i] = -1; 
-
-	// pick each process and find suitable blocks 
-	// according to its size ad assign to it 
-	for (int i = 0; i < n; i++) 
-	{ 
-		// Find the best fit block for current process 
-		int bestIdx = -1; 
-		for (int j = 0; j < m; j++) 
-		{ 
-			if (blockSize[j] >= processSize[i]) 
-			{ 
-				if (bestIdx == -1) 
-					bestIdx = j; 
-				else if (blockSize[bestIdx] > blockSize[j]) 
-					bestIdx = j; 
-			} 
-		} 
-
-		// If we could find a block for current process 
-		if (bestIdx != -1) 
-		{ 
-			// allocate block j to p[i] process 
-			allocation[i] = bestIdx; 
-
-			// Reduce available memory in this block. 
-			blockSize[bestIdx] -= processSize[i]; 
-		} 
-	} 
-
-	cout << "\nProcess No.\tProcess Size\tBlock no.\n"; 
-	for (int i = 0; i < n; i++) 
-	{ 
-		cout << " " << i+1 << "\t\t" << processSize[i] << "\t\t"; 
-		if (allocation[i] != -1) 
-			cout << allocation[i] + 1; 
-		else
-			cout << "Not Allocated"; 
-		cout << endl; 
-	} 
-} 
-
-// Driver Method 
-int main() 
-{ 
-	int blockSize[]={100,500,200,300,600};
-	
-	
-	int processSize[]={212,417,112,426};
-
-	int m = sizeof(blockSize) / sizeof(blockSize[0]); 
-	int n = sizeof(processSize) / sizeof(processSize[0]); 
-
-	bestFit(blockSize, m, processSize, n); 
-
-	return 0 ; 
+int main(){
+    int i,j,block[10],process[10],n1,n2,check;
+    cout<<"Enter the number of memory blocks";
+    cin>>n1;
+    cout<<"Enter the number of processes :";
+    cin>>n2;
+    for(i=0;i<n1;i++){
+        cout<<"Enter memeory block "<<i+1<<":";
+        cin>>block[i];
+    }
+    for(i=0;i<n2;i++){
+        cout<<"Enter the memory required by process "<<i+1<<":";
+        cin>>process[i];
+    }
+    cout<<"Process\tProcess Size\tMemory Block"<<endl;
+    for(i=0;i<n2;i++){
+        check=0;
+        for(j=0;j<n1;j++){
+             if(process[i]<=block[j]){
+                 block[j]=block[j]-process[i];
+                 check=1;
+                 break;
+             }
+        }
+        if(check!=1){
+            cout<<i+1<<"\t\t"<<process[i]<<"\t\t"<<"Not allocated"<<endl;
+        }
+        else{
+            cout<<i+1<<"\t\t"<<process[i]<<"\t\t"<<j+1<<endl;
+        }
+    }
 }
